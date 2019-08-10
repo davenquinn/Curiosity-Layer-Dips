de7fd02bb8faf06c2865b7833e637ebdb45c39cb
Daven Quinn <dev@davenquinn.com>
2019-08-09 17:14:47 -0700

    Extract shapes from DEM

--- new-data/extract-shapes.py
+++ new-data/extract-shapes.py
@@ -3,6 +3,7 @@
 import rasterio
 from fiona import collection
 from shapely.geometry import shape
+from shapely.ops import transform
 import numpy as N
 from affine import Affine
 
@@ -17,36 +18,35 @@ features = []
 
 def extract_feature(feature, dem, embed=False):
     geom = shape(feature['geometry'])
+    # QGIS uses a negative Y axis for pixel coordinates
+    tfunc = lambda x,y: (x,-y)
+    geom = transform(tfunc, geom)
 
     # We don't support multiband extraction yet
     x = extract_shape(geom, dem, band=1)
     y = extract_shape(geom, dem, band=2)
     z = extract_shape(geom, dem, band=3)
 
-    # Z in site frame is apparently down, whose idea was that?
+    # Z in Curiosity site frame is apparently down, whose idea was that?
     z *= -1
 
     ix = N.index_exp[:,-1]
     xyz = N.column_stack((x[ix], y[ix], z[ix]))
 
-    xyz = xyz[xyz.sum(axis=1) != 0]
+    #xyz = xyz[xyz.sum(axis=1) != 0]
     return xyz
 
 
 # QGIS gives us points with negative y axis pixels
-px_affine = Affine(1,0,0,0,-1,0)
-with rasterio.open(XYZFILE,'r', transform=px_affine) as outcrop:
+with rasterio.open(XYZFILE,'r') as outcrop:
     with collection(LINES, 'r') as f:
         # Everything should be in pixel coordinates
-
-
         for feature in f:
             xyz = extract_feature(feature, outcrop)
             features.append(xyz)
 
     with collection(POLYS, 'r') as f:
         # Everything should be in pixel coordinates
-
         for feature in f:
             xyz = extract_feature(feature, outcrop)
             features.append(xyz)
