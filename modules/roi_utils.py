74aeeb30b1534be5a1b5ce8eb7f08d9b78df88bb
Daven Quinn <dev@davenquinn.com>
2019-09-09 04:12:35 -0400

    Updated coordinate transformation from site frame

--- modules/roi_utils.py
+++ modules/roi_utils.py
@@ -2,6 +2,27 @@ from pandas import read_csv
 from numpy import split
 from click import secho
 
+def swap_columns(df, c1, c2):
+    df.loc[:,"__"] = df.loc[:,c2]
+    df.loc[:,c2] = df.loc[:,c1]
+    df.loc[:,c1] = df.loc[:,"__"]
+    return df.drop("__", axis=1)
+
+def site_frame_to_global(df):
+    """
+    Convert Curiosity site frame coordinates to global frame
+    coordinates suitable for use in GIS programs.
+
+    Site frame: x North, y East, z Down
+    Global frame: x East, y North, z Up
+    """
+
+    # Swap x and y axes, and invert z
+    df = swap_columns(df,'x','y')
+    df = swap_columns(df,'xe','ye')
+    df.loc[:,"z"] *= -1
+    return df
+
 def read_roi(fn, names):
     df = read_csv(str(fn),
             delim_whitespace=True,
@@ -10,17 +31,17 @@ def read_roi(fn, names):
             names=names.split())
     frames = split(df, df[df.isnull().all(1)].index)
     for df in frames:
+        df.drop(columns=["ID"], inplace=True)
+        df.dropna(how='all', inplace=True)
+
         # Convert pixel coords to integer for simplicity
         df.loc[:,'X'] = df.loc[:,'X'].astype(int)
         df.loc[:,'Y'] = df.loc[:,'Y'].astype(int)
-
-        df.drop(columns=["ID"], inplace=True)
         df.set_index(["X","Y"], inplace=True)
-        df.dropna(how='all', inplace=True)
 
     return frames
 
-def import_stereo_roi(xyz_path, xye_path=None, default_error=0.005):
+def __import_stereo_roi(xyz_path, xye_path=None, default_error=0.005):
     """
     Import a Curiosity MastCam stereo ROI from ENVI 5 ROIs
     """
@@ -39,3 +60,7 @@ def import_stereo_roi(xyz_path, xye_path=None, default_error=0.005):
             # Set error to a low symmetrical value
             df['xe'] = df['ye'] = df['ze'] = default_error
             yield df
+
+def import_stereo_roi(*args, **kwargs):
+    for df in __import_stereo_roi(*args,**kwargs):
+        yield site_frame_to_global(df)
