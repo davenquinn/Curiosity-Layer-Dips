f2acb8d32186a6279835244c2f44934cdad3674a
Daven Quinn <dev@davenquinn.com>
2019-09-12 22:02:03 -0500

    git-annex in Daven@Daven-Quinn:~/Projects/Curiosity-Layer-Dips

--- spec.js
+++ spec.js
@@ -1,9 +1,5 @@
 
 const printer = new Printer({buildDir: __dirname+"/output"});
-printer.task('attitudes-1s.pdf', './sol-plots/index.coffee', {stacked: false, type: '1s'});
-printer.task('attitudes-1s-stacked.pdf', './sol-plots/index.coffee', {stacked: true, type: '1s'});
-printer.task('attitudes-2s.pdf', './sol-plots/index.coffee', {stacked: false, type: '2s'});
-printer.task('attitudes-2s-stacked.pdf', './sol-plots/index.coffee', {stacked: true, type: '2s'});
 
 for (let type of ['no-error', 'weighted', 'monte-carlo', 'monte-carlo-3s', 'monte-carlo-5s']) {
   printer.task(`roi-plots/attitudes-${type}.pdf`, './roi-plots/index.coffee', {type});
